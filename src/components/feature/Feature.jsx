f287381e56169eb98c6e9fa69ed739a78b666fb2
Puchana <puchana123@gmail.com>
2023-04-10 17:44:52 +0700

    UI-WhatGPT3

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({title,text,strecth}) => {
+  let isStrecth = false
+  if(strecth === true) isStrecth=true
+  
   return (
-    <div>Feature</div>
+    <>
+    {isStrecth?
+     ( <div className='gpt3__features-container__feature'>
+      <div className="gpt3__features-container__feature-title">
+        <div/>
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">
+        <p>{text}</p> 
+      </div>
+    </div>)
+    :
+      <div className='gpt3__features-container__feature-col'>
+      <div className="gpt3__features-container__feature-title">
+        <div/>
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">
+        <p>{text}</p> 
+      </div>
+    </div>
+    }
+    </>
   )
 }
 

22c25ebe9ad522ff899451c6779b2f1727c54ce1
Puchana <puchana123@gmail.com>
2023-04-11 16:18:58 +0700

    UI-footer

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import './header.css'
-import people from '../../assets/people.png'
-import ai from '../../assets/ai.png'
+import React from "react";
+import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
-    <div className='gpt3__header section__padding' id='home'>
+    <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
-        <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim dolore illo vitae debitis? Natus accusantium modi sit, exercitationem explicabo saepe aut, cum ipsam nisi id praesentium illum quasi dolorem. Amet!</p>
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
 
         <div className="gpt3__header-content__input">
-          <input type="email" placeholder='Your Email Address' />
-          <button type='button'>Get Started</button>
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
         </div>
 
         <div className="gpt3__header-content__people">
@@ -21,10 +28,10 @@ const Header = () => {
         </div>
       </div>
       <div className="gpt3__header-image">
-          <img src={ai} alt="ai" />
-        </div>
+        <img src={ai} alt="ai" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

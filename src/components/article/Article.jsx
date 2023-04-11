00decd1fa534d1145e37f5faa0d4e6522be01b36
Puchana <puchana123@gmail.com>
2023-04-11 15:39:00 +0700

    UI-Blog

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
+const Article = ({imgURL, date, text}) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgURL} alt="blog Image" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
